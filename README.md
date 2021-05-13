50302ec026550d34641ecb3bd06a7759bd2c775c
uzuegbu joseph <josephuzuegbu55@gmail.com>
2021-05-13 21:03:34 +0100

    preview images added to README.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # Image Compression App
 Introduction: is an app that compress files without loosing it quality or originality.  Settles the challenge of downloading by enabling file download to the local computer. Compressed file size is display along with the uncompressed file Size for disparity.
 
+## Project preview image
+<img src='images\Capture.PNG'>
+
+<img src='images\Capture2.PNG'>
+
+## Project Link
+[Go to the project](https://image-compression123.netlify.app/)
+
 ## language
 1. HTML
 2. CSS
--- script.js
+++ script.js
@@ -77,24 +77,6 @@ function calculateSize(Img, maxWidth, maxHeight) {
     return [width, height];
   }
 
-// function calculaterequiredWidthHeight(Img, max_Height, max_width){
-//     let imgwidth =  Img.width;
-//     let imgheight = Img.height;
-
-//     if (imgwidth > max_width) {
-//         if (imgwidth > imgheight) {
-//            imgwidth = max_width;
-//            imgheight = Math.round((imgheight * max_Height)/imgwidth); 
-//         }
-//     }else{
-//         if (imgheight > max_Height) {
-//                 imgheight = max_Height;
-//                 imgwidth = Math.round((imgwidth * max_width)/imgheight);
-//         }
-//     }
-
-//     return [imgheight, imgwidth];
-// }
 
 
 
@@ -115,76 +97,3 @@ function calculateSize(Img, maxWidth, maxHeight) {
 
 
 
-
-
-// const MAX_WIDTH = 320;
-// const MAX_HEIGHT = 180;
-// const MIME_TYPE = "image/jpeg";
-// const QUALITY = 0.7;
-
-// const input = document.getElementById("file");
-// input.onchange = function (ev) {
-//   const file = ev.target.files[0]; // get the file
-//   const blobURL = URL.createObjectURL(file);
- 
-//   const img = new Image();
-//   img.src = blobURL;
-//   img.onerror = function () {
-//     URL.revokeObjectURL(this.src);
-//     // Handle the failure properly
-//     console.log("Cannot load image");
-//   };
-//   img.onload = function () {
-//     URL.revokeObjectURL(this.src);
-//     const [newWidth, newHeight] = calculateSize(img, MAX_WIDTH, MAX_HEIGHT);
-//     const canvas = document.createElement("canvas");
-//     canvas.width = newWidth;
-//     canvas.height = newHeight;
-//     const ctx = canvas.getContext("2d");
-//     ctx.drawImage(img, 0, 0, newWidth, newHeight);
-//     canvas.toBlob(
-//       (blob) => {
-//         // Handle the compressed image. es. upload or save in local state
-//         displayInfo('Original file', file);
-//         displayInfo('Compressed file', blob);
-//       },
-//       MIME_TYPE,
-//       QUALITY
-//     );
-//     canvas_container.append(canvas);
-//   };
-// };
-
-// function calculateSize(img, maxWidth, maxHeight) {
-//   let width = img.width;
-//   let height = img.height;
-
-//   // calculate the width and height, constraining the proportions
-//   if (width > height) {
-//     if (width > maxWidth) {
-//       height = Math.round((height * maxWidth) / width);
-//       width = maxWidth;
-//     }
-//   } else {
-//     if (height > maxHeight) {
-//       width = Math.round((width * maxHeight) / height);
-//       height = maxHeight;
-//     }
-//   }
-//   return [width, height];
-// }
-
-// // Utility functions for demo purpose
-
-// function displayInfo(label, file) {
-//   const p = document.createElement('p');
-//   p.innerText = `${label} - ${readableBytes(file.size)}`;
-//   document.getElementById('imgcanvas_container').append(p);
-// }
-
-// function readableBytes(bytes) {
-//   const i = Math.floor(Math.log(bytes) / Math.log(1024)),
-//     sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
-
-//   return (bytes / Math.pow(1024, i)).toFixed(2) + ' ' + sizes[i];
-// }
